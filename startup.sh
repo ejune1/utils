5e4f3fc30a2c4b7fb03cdb1b8324360668231f9d
ejune1 <ejune1@asu.edu>
2024-10-04 17:43:30 +0000

    docker stuff

--- startup.sh
+++ startup.sh
@@ -35,10 +35,39 @@ git clone https://github.com/ejune1/test.git test
 alias STAT='git status'
 alias vi='vim'
 
-#grpc
+#docker
+#remove all docker
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+#docker GPC key
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+#add repo
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+#install
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+#uninstall / remove all containers and images
+sudo apt-get purge docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-ce-rootless-extras
+sudo rm -rf /var/lib/docker
+sudo rm -rf /var/lib/containerd
+#cmds
+sudo docker build .
+sudo docker images
+sudo docker run <image>
+sudo docker exec -it <container> /bin/bash
+
+#python virtual env
 python3 -m venv .venv
 source .venv/bin/activate
 #deactivate
+
+#grpc
 python3 -m pip install grpcio
 python3 -m pip install grpcio-tools
 python -m grpc_tools.protoc -I../proto --python_out=. --pyi_out=. --grpc_python_out=. ../proto/rando.proto
